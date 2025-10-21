d4ff6293b746919df3368af5d5f921b545b38ca8
Mishbringer <vip.kukov@bk.ru>
2025-10-21 07:06:57 +0500

    task3 solve

---
+++ task03/src/com/example/task03/ComplexNumber.java
@@ -0,0 +1,23 @@
+package com.example.task03;
+
+public class ComplexNumber {
+    private final double realPart;
+    private final double imaginaryPart;
+
+    ComplexNumber(double re, double im) {
+        this.realPart = re;
+        this.imaginaryPart = im;
+    }
+
+    ComplexNumber add(ComplexNumber num2) {
+        return new ComplexNumber(realPart + num2.realPart, imaginaryPart + num2.imaginaryPart);
+    }
+
+    ComplexNumber multiply(ComplexNumber num2) {
+        return new ComplexNumber(realPart * num2.realPart - imaginaryPart * num2.imaginaryPart, realPart * num2.imaginaryPart + imaginaryPart * num2.realPart);
+    }
+
+    public String ToString() {
+        return String.format("%f %s %fi", realPart, (imaginaryPart < 0 ? "-" : "+"), Math.abs(imaginaryPart));
+    }
+}
